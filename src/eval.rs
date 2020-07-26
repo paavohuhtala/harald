74d0297eb79cccd812e4568d69ec03e1a6b30bbd
Paavo Huhtala <paavo.huhtala@gmail.com>
2020-07-26 21:13:23 +0300

    Coerce pattern parts into strings

--- src/eval.rs
+++ src/eval.rs
@@ -7,6 +7,7 @@ use thiserror::Error;
 pub enum Value<'a> {
     StringV(Cow<'a, str>),
     BagV(&'a Bag),
+    TableDictV(&'a TableDict),
 }
 
 impl<'a> Value<'a> {
@@ -14,6 +15,7 @@ impl<'a> Value<'a> {
         match self {
             Value::StringV(_) => "string",
             Value::BagV(_) => "bag",
+            Value::TableDictV(_) => "table_dict",
         }
     }
 
@@ -32,7 +34,22 @@ impl<'a> Display for Value<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Value::StringV(v) => f.write_str(v),
-            Value::BagV(bag) => write!(f, "[Bag {}]", bag.id),
+            Value::BagV(bag) => write!(
+                f,
+                "bag ({})",
+                (bag.name)
+                    .as_ref()
+                    .map(|n| n.as_str())
+                    .unwrap_or("anonymous")
+            ),
+            Value::TableDictV(table_dict) => write!(
+                f,
+                "table_dict ({})",
+                (table_dict.name)
+                    .as_ref()
+                    .map(|n| n.as_str())
+                    .unwrap_or("anonymous")
+            ),
         }
     }
 }
@@ -44,6 +61,7 @@ pub struct Pattern {
 
 #[derive(Debug)]
 pub struct TableDict {
+    name: Option<String>,
     bags: HashMap<String, Bag>,
 }
 
@@ -61,6 +79,21 @@ pub enum Expression {
     VariableE(String),
     PatternE(Pattern),
     BagE(Bag),
+    TableDictE(TableDict),
+}
+
+impl Expression {
+    pub fn name_hint(&mut self, name: String) {
+        match self {
+            Expression::BagE(bag) => {
+                bag.name = Some(name);
+            }
+            Expression::TableDictE(table_dict) => {
+                table_dict.name = Some(name);
+            }
+            _ => {}
+        }
+    }
 }
 
 pub trait StringWritable {
@@ -80,6 +113,11 @@ pub enum InterpreterError {
     #[error("Unexpected type: expected {expected}, was {was}")]
     UnexpectedType {
         expected: &'static str,
+        was: &'static str,
+    },
+    #[error("Value of type {was} cannot be coerced to {target}")]
+    CoercionError {
+        target: &'static str,
         was: &'static str,
     },
 }
@@ -153,6 +191,26 @@ impl CompiledScript {
             .map(Cow::into_owned)
     }
 
+    pub fn try_coerce_to_string<'a>(
+        &'a self,
+        value: Value<'a>,
+    ) -> Result<Cow<'a, str>, InterpreterError> {
+        match value {
+            Value::StringV(v) => Ok(v),
+            Value::BagV(bag) => {
+                let mut rng = rand::thread_rng();
+                let i = bag.distribution.sample(&mut rng);
+                let expression = &bag.items[i];
+                let value = self.eval_expression(expression)?;
+                self.try_coerce_to_string(value)
+            }
+            otherwise => Err(InterpreterError::CoercionError {
+                target: "string",
+                was: otherwise.get_type_name(),
+            }),
+        }
+    }
+
     pub fn eval_expression<'a>(
         &'a self,
         expression: &'a Expression,
@@ -171,19 +229,15 @@ impl CompiledScript {
                 let mut combined = String::new();
 
                 for part in &pattern.parts {
-                    let part = self.eval_expression(part)?.try_as_string()?;
-                    combined.push_str(&part);
+                    let part = self.eval_expression(part)?;
+                    let part_as_string = self.try_coerce_to_string(part)?;
+                    combined.push_str(&part_as_string);
                 }
 
                 Ok(Value::StringV(Cow::from(combined)))
             }
-            Expression::BagE(bag) => {
-                let mut rng = rand::thread_rng();
-                let i = bag.distribution.sample(&mut rng);
-                let expression = &bag.items[i];
-
-                self.eval_expression(expression)
-            }
+            Expression::BagE(bag) => Ok(Value::BagV(bag)),
+            Expression::TableDictE(table_dict) => Ok(Value::TableDictV(table_dict)),
         }
     }
 
@@ -194,7 +248,9 @@ impl CompiledScript {
     pub fn add_statement(&mut self, statement: ast::Statement) {
         match statement {
             ast::Statement::AssignmentS(assignment) => {
-                let expression = self.transform_expression(*assignment.value);
+                let mut expression = self.transform_expression(*assignment.value);
+                expression.name_hint(assignment.name.clone());
+
                 self.define_variable(assignment.name, expression);
             }
         }
